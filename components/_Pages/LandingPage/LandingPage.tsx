c6d10a112d9424ef5aa771362d56e75183e7a5b1
Joe Szaf <joeszaf+github@gmail.com>
2023-03-04 16:35:49 -0500

    scaffold hero section

--- components/_Pages/LandingPage/LandingPage.tsx
+++ components/_Pages/LandingPage/LandingPage.tsx
@@ -1,9 +1,11 @@
 import FollowUsSection from '../../FollowUsSection/FollowUsSection';
+import HeroSection from '../../HeroSection/HeroSection';
 import MailingListSection from '../../MailingListSection/MailingListSection';
 
 const LandingPage = () => {
   return (
     <main>
+      <HeroSection />
       <FollowUsSection />
       <MailingListSection />
     </main>
