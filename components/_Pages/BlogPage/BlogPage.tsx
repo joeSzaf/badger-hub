15bf1e72bb9e9d03b6634ef4c28d77fccef16930
Joe Szaf <joeszaf+github@gmail.com>
2023-03-05 23:05:19 -0500

    scaffold blog page

---
+++ components/_Pages/BlogPage/BlogPage.tsx
@@ -0,0 +1,11 @@
+import PageTitle from 'components/_Atomic/PageTitle/PageTitle';
+
+const BlogPage = () => {
+  return (
+    <main>
+      <PageTitle copy='Badger Blog' />
+    </main>
+  );
+};
+
+export default BlogPage;
