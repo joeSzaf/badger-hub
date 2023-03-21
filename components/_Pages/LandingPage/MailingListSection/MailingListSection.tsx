83b230f775344fab583afb059be251afcf8c80fd
Joe Szaf <joeszaf+github@gmail.com>
2023-03-20 20:32:38 -0400

    add mailing list link

--- components/_Pages/LandingPage/MailingListSection/MailingListSection.tsx
+++ components/_Pages/LandingPage/MailingListSection/MailingListSection.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@mui/material';
 import PageSection from 'components/_Atomic/PageSection/PageSection';
+import Link from 'next/link';
 import { MailingListWrapper } from './MailingListSection.style';
 
 const MailingListSection = () => {
@@ -7,7 +8,15 @@ const MailingListSection = () => {
     <PageSection backgroundColor='2b2b2b' textColor='ffffff'>
       <MailingListWrapper>
         <h2>Let us tell you about all the cool stuff we are working on!</h2>
-        <Button variant='outlined'>Sign up for a cool mailing list</Button>
+        <Button
+          href='http://eepurl.com/ini0zI'
+          LinkComponent={Link}
+          rel='noopener noreferrer'
+          target='_blank'
+          variant='outlined'
+        >
+          Sign up for a cool mailing list
+        </Button>
       </MailingListWrapper>
     </PageSection>
   );
